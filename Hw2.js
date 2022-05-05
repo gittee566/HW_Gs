51f741e672b50151fdc2ccd5f7878ed6f1040e14
Teegit <teerasak.hs@gmail.com>
2022-05-05 23:28:19 +0700

    edit Hw2

---
+++ Hw2.js
@@ -0,0 +1,16 @@
+let AA = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+let BB = [];
+console.log([AA]);
+x
+
+for (let i = 0; i < AA.length; i++) {
+
+    if (i % 2 === 0) {
+        BB[i + 1] = AA[i]
+    } else {
+        BB[i - 1] = AA[i]
+    }
+
+}
+
+process.stdout.write(BB.toString());
